ac92c82245bab1b09b53d16f5af6dffda39de196
Jan Erik Andersen <jan.erik.andersen@spk.no>
2020-04-24 00:12:43 +0200

    new vagrantfile

--- defaults/vagrant.rb
+++ defaults/vagrant.rb
@@ -1,14 +1,13 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-#MANAGER       = "k8s-manager"
-CPU           = 2
-RAM           = 1024
-DOMAIN        = ".example.com"
-#MANAGER_IP    = "192.168.42.10"
-NODE_NET      = "192.168.42."
-NETMASK       = "255.255.255.0"
-IMAGE_NAME    = "bento/ubuntu-18.04"
-PLAYBOOK      = "./ansible/testEnv.yml"
-NODES         = 1
-MACHINE_ID    = 0
+CPU             = 2
+RAM             = 1024
+DOMAIN          = ".example.com"
+NODE_NET        = "192.168.42."
+NETMASK         = "255.255.255.0"
+IMAGE_NAME      = "bento/ubuntu-18.04"
+PLAYBOOK        = "./ansible/managers.yml"
+PLAYBOOK_NODES  = "./ansible/workerNodes.yml"
+NODES           = 1
+MACHINE_ID      = 0
