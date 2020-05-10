b8b62ff6c595d6e32486cd0183fddd882b549713
Jan Erik Andersen <jan.erik.andersen@spk.no>
2020-05-10 09:56:49 +0200

    Started to work on main.tf

--- kvm/main.tf
+++ kvm/main.tf
@@ -1,4 +1,16 @@
+# instance the provider
 provider "libvirt" {
   uri = "qemu:///system"
 }
 
+resource "libvirt_domain" "k8s_masters" {
+  name = "k8s_masters"
+  vcpu = 1
+  memory = 1024
+}
+
+resource "libvirt_domain" "k8s_workers" {
+  name = "k8s_workers"
+  vcpu = 2
+  memory = 1024
+}
